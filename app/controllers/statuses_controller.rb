a1442555273e4d022ffa07410801722515fd1531
JBader89 <JBader@conncoll.edu>
2013-07-24 22:45:55 -0400

    Add status and test validations.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,5 @@
 class StatusesController < ApplicationController
-  before_filter :authenticate_user!, only: [:new]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :delete]
 
   # GET /statuses
   # GET /statuses.json
